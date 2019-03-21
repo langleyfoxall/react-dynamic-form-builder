18effc90ea844268ad4e3e147d90972dace84402
Dexter Marks-Barber <dexter.mb@outlook.com>
2019-03-21 10:29:45 +0000

    Flip expressions

--- src/DynamicFormBuilder.jsx
+++ src/DynamicFormBuilder.jsx
@@ -38,10 +38,10 @@ class DynamicFormBuilder extends React.Component {
                 }
 
                 if (typeof value === 'string' || Array.isArray(value)) {
-                    return !value.length;
+                    return !!value.length;
                 }
 
-                return value === null || value === undefined;
+                return value !== null && value !== undefined;
             },
             email: value => (
                 /^$|^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value)
